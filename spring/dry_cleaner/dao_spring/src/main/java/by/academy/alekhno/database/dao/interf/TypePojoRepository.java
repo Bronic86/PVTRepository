6448b60eb16671a83159d3f62e5fb2f3b78d126d
Boris <Ezhik_b_tymane@mail.ru>
2015-09-28 18:19:43 +0300

    change typePojoRepository method

--- spring/dry_cleaner/dao_spring/src/main/java/by/academy/alekhno/database/dao/interf/TypePojoRepository.java
+++ spring/dry_cleaner/dao_spring/src/main/java/by/academy/alekhno/database/dao/interf/TypePojoRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import by.academy.alekhno.spring.pojo.TypePojo;
-import by.academy.alekhno.spring.pojo.UserPojo;
 
 @Repository("typePojoRepository")
-public interface TypePojoRepository extends JpaRepository<UserPojo, Integer> {
+public interface TypePojoRepository extends JpaRepository<TypePojo, Integer> {
 
 	@Query("SELECT t FROM TypePojo t WHERE t.name like :type_name")
 	TypePojo getByName(@Param("type_name") String name);
